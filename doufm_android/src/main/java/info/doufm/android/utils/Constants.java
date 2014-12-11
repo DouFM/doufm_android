e9948bdc24d7d46172c5958e695df2ee7459e0fb
QichaoChen <2009chenqc@163.com>
2014-12-12 04:14:21 +0800

    Refactor Color Theme to Constants.java

--- doufm_android/src/main/java/info/doufm/android/utils/Constants.java
+++ doufm_android/src/main/java/info/doufm/android/utils/Constants.java
@@ -7,4 +7,19 @@ package info.doufm.android.utils;
 public class Constants {
     public static final int DISMISS = 1000;
     public static final int UPDATE_TIME = 2000;
+    //Meterial Design主题(500 300 100)
+    public static String[] mActionBarColors = {"#607d8b", "#ff5722", "#795548",
+            "#ffc107", "#ff9800", "#259b24",
+            "#8bc34a", "#cddc39", "#03a9f4",
+            "#00bcd4", "#009688", "#673ab7",
+            "#673ab7", "#3f51b5", "#5677fc",
+            "#e51c23", "#e91e63", "#9c27b0",
+            "#607d8b"};
+    public static String[] mBackgroundColors = {"#90a4ae", "#ff8a65", "#a1887f",
+            "#ffd54f", "#ffb74d", "#42bd41",
+            "#aed581", "#dce775", "#4fc3f7",
+            "#4dd0e1", "#4db6ac", "#9575cd",
+            "#9575cd", "#7986cb", "#91a7ff",
+            "#f36c60", "#f06292", "#ba68c8",
+            "#90a4ae"};
 }
