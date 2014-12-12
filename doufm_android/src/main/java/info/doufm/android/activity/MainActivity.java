9e03d450b6d12bd00389bf99d322d5153bcdb995
QichaoChen <2009chenqc@163.com>
2014-12-12 23:28:34 +0800

    add umeng analytics it is big move for an app

--- doufm_android/src/main/java/info/doufm/android/activity/MainActivity.java
+++ doufm_android/src/main/java/info/doufm/android/activity/MainActivity.java
@@ -35,6 +35,7 @@ import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.ikimuhendis.ldrawer.ActionBarDrawerToggle;
 import com.ikimuhendis.ldrawer.DrawerArrowDrawable;
+import com.umeng.analytics.MobclickAgent;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -371,6 +372,7 @@ public class MainActivity extends Activity implements MediaPlayer.OnCompletionLi
     @Override
     protected void onResume() {
         super.onResume();
+        MobclickAgent.onResume(this);
         mListLisener = new ListListener();
         if (isFirstLoad) {
             getMusicList();
@@ -645,6 +647,7 @@ public class MainActivity extends Activity implements MediaPlayer.OnCompletionLi
     protected void onPause() {
         super.onPause();
         RequestManager.getRequestQueue().cancelAll(this);
+        MobclickAgent.onPause(this);
     }
 
     @Override
