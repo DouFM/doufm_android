0d7c1fc30a673f2806ce5f51baeb9b7aa1d41d11
QichaoChen <2009chenqc@163.com>
2014-12-17 23:41:31 +0800

    add LoginActivity and UserActivity for user func,and add md5 func

--- doufm_android/src/main/java/info/doufm/android/user/UserUtil.java
+++ doufm_android/src/main/java/info/doufm/android/user/UserUtil.java
@@ -13,6 +13,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -425,5 +428,29 @@ public class UserUtil {
                     }
                 })
         );
+    }
+
+    /**
+     * 生成全小写的MD5值
+     *
+     * @param password
+     * @return
+     */
+    public static String ToLowerCaseMD5(String password) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(password.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 should be supported?", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(" UTF-8 should be supported?", e);
+        }
+
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString().toLowerCase();
     }
 }
